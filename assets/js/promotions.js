def270d5db9b976173331b722979c4e308c82ca9
Jorgito Ramírez Galvan <jorgitoram@Jorgitos-MacBook-Air.local>
2023-10-19 17:57:03 +0200

    added promotion sec

--- assets/js/promotions.js
+++ assets/js/promotions.js
@@ -1,39 +1,39 @@
-async function dishes_generator() {
-    fetch('../assets/js/dishes.json')
+async function promotions_generator() {
+    fetch('../assets/js/promotions.json')
         .then(response => response.json())
         .then(data => {
-            let menu_section = document.getElementById("menu")
+            let menu_section = document.getElementById("promotions")
 
-            for (dish in data) {
+            for (promo in data) {
 
                 let card = document.createElement("div")
                 card.classList.add("card")
 
                 let dish_img = document.createElement("img")
-                dish_img.src = data[dish]["img_url"]
+                dish_img.src = data[promo]["img_url"]
                 dish_img.setAttribute("height", "40%")
 
                 let dish_title = document.createElement("h3")
                 dish_title.classList.add("dish_name", "text_box_weight")
-                let dish_title_text = document.createTextNode(`${data[dish]["name"]} - ${data[dish]["price"]}$`)
+                let dish_title_text = document.createTextNode(`${data[promo]["name"]} - ${data[promo]["price"]}$`)
                 dish_title.appendChild(dish_title_text)
 
                 let arrow = document.createElement("img")
                 arrow.src = "../assets/img/home/dishes/arrow-icon.png"
                 arrow.classList.add("arrow")
                 arrow.setAttribute("width", "7%")
-                arrow.setAttribute("id", dish)
-                arrow.setAttribute("onclick", `text_appear("d${dish}", "${dish}")`)
+                arrow.setAttribute("id", promo)
+                arrow.setAttribute("onclick", `text_appear("d${promo}", "${promo}")`)
 
                 let dish_desc = document.createElement("div")
                 dish_desc.classList.add("dish_description")
-                dish_desc.setAttribute("id", `d${dish}`)
-                let dish_desc_text = document.createTextNode(`${data[dish]["description"]}`)
+                dish_desc.setAttribute("id", `d${promo}`)
+                let dish_desc_text = document.createTextNode(`${data[promo]["description"]}`)
                 dish_desc.appendChild(dish_desc_text)
                 let buy_btn = document.createElement("button")
                 buy_btn.classList.add("addCart")
                 buy_btn.textContent = "Buy"
-                buy_btn.setAttribute("onclick", `addCart("${dish}")`)
+                buy_btn.setAttribute("onclick", `addCart("${promo}")`)
                 dish_desc.appendChild(buy_btn)
 
                 card.appendChild(dish_img)
@@ -50,4 +50,4 @@ async function dishes_generator() {
         });
 }
 
-dishes_generator();
+promotions_generator();
